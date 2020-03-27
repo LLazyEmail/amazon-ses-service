f478ba69ca05356efad5fa4353be498534d1880b
Vadym Putrov <vadym.putrov@chisw.com>
2020-03-27 20:01:14 +0200

    add html

--- server.js
+++ server.js
@@ -9,6 +9,50 @@ const SESConfig = {
     region: process.env.AWS_REGION
 }
 
+function generateHTMLInquiryEmail(email, message) {
+    return `
+     <!DOCTYPE html>
+     <html>
+       <head>
+         <meta charset='UTF-8' />
+         <title>title</title>
+       </head>
+       <body>
+        <table border='0' cellpadding='0' cellspacing='0' height='100%' width='100%' id='bodyTable'>
+         <tr>
+             <td align='center' valign='top'>
+                 <table border='0' cellpadding='20' cellspacing='0' width='600' id='emailContainer'>
+                     <tr style='background-color:#99ccff;'>
+                         <td align='center' valign='top'>
+                             <table border='0' cellpadding='20' cellspacing='0' width='100%' id='emailBody'>
+                                 <tr>
+                                     <td align='center' valign='top' style='color:#337ab7;'>
+                                         <h3>${message}</h3>
+                                     </td>
+                                 </tr>
+                             </table>
+                         </td>
+                     </tr>
+                     <tr style='background-color:#74a9d8;'>
+                         <td align='center' valign='top'>
+                             <table border='0' cellpadding='20' cellspacing='0' width='100%' id='emailReply'>
+                                 <tr style='font-size: 1.2rem'>
+                                     <td align='center' valign='top'>
+                                         <span style='color:#286090; font-weight:bold;'>Send From:</span> <br/> ${email}
+                                     </td>
+                                 </tr>
+                             </table>
+                         </td>
+                     </tr>
+                 </table>
+             </td>
+         </tr>
+         </table>
+       </body>
+     </html>
+    `
+}
+
 const params = {
     Destination: {
         ToAddresses: addresses
@@ -17,8 +61,7 @@ const params = {
         Body: {
             Html: {
                 Charset: 'UTF-8',
-                Data:
-                    'This message body contains HTML formatting, like <a class="ulink" target="_blank">hi</a>.'
+                Data: generateHTMLInquiryEmail("vadim.putrov", 'All is ok')
             },
             Text: {
                 Charset: 'UTF-8',
@@ -34,15 +77,15 @@ const params = {
     Source: process.env.SOURCE
 };
 
-const sendEmail = () =>{
-    try{
+const sendEmail = () => {
+    try {
         //call one send Email for one recipients
         //@TODO improve this code
-        new AWS.SES(SESConfig).sendEmail(params).promise().then((res)=>{
+        new AWS.SES(SESConfig).sendEmail(params).promise().then((res) => {
             console.log("res", res);
         })
-        
-    }catch{
+
+    } catch{
         console.log('err');
     }
 };

9a88c22148ef37608e010849c714eab03b1af5d1
Vadim <vadim.putrov@gmail.com>
2020-03-29 13:29:24 +0300

    add getting templates from ses

--- server.js
+++ server.js
@@ -48,19 +48,23 @@ const sendEmail = () => {
     }
 };
 
-sendEmail()
+// sendEmail()
 
-// const getListTemplates = () => {
-//     try {
-//         let templatePromise = new AWS.SES(SESConfig).listTemplates({ MaxItems: 5 }).promise();
-//         templatePromise.then((data) => {
-//             console.log("data", data);
-
-//         }).catch((err) =>{
-//             console.log(err);
-//         })
-//     } catch{
-//         console.log("error");
+const getListTemplates = async () => {
+    try {
+        let templatePromise = await new AWS.SES(SESConfig).listTemplates({ MaxItems: 5 }).promise();
+        console.log(templatePromise);
         
-//     }
-// }
+        // templatePromise.then((data) => {
+        //     console.log("data", data);
+
+        // }).catch((err) =>{
+        //     console.log(err);
+        // })
+    } catch{
+        console.log("error");
+        
+    }
+}
+
+// getListTemplates()

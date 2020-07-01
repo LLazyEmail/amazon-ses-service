662da386d8ed7287cd185df523f85ce05ad64a86
Vadim <vadim.putrov@gmail.com>
2020-07-01 20:55:04 +0300

    update file-loader

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,7 +1,9 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
 import webpack from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+// import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
 import WebpackMd5Hash from 'webpack-md5-hash';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 const path = require('path');
@@ -15,6 +17,7 @@ export default {
   resolve: {
     extensions: ['.js', '.jsx', '.css']
   },
+  mode: 'production',
   context: path.resolve(process.cwd(), 'client'),
   devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   entry: './index',
@@ -34,7 +37,10 @@ export default {
     new webpack.DefinePlugin(GLOBALS),
 
     // Generate an external css file with a hash in the filename
-    new ExtractTextPlugin('[name].[contenthash].css'),
+    // new ExtractTextPlugin('[name].[contenthash].css'),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
 
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
@@ -67,17 +73,40 @@ export default {
     // new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
-      rules: [
-        {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
-        {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
-        {test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf)$/, loader: 'url-loader', options: { name: '[hash].[ext]', limit: 10000 }},
-        {test: /\.ico$/, loader: 'file-loader', options: { name: '[hash].[ext]' }},
-        {test: /(\.css|\.scss)$/, loaders: ['style-loader', 'css-loader',  'sass-loader']},
-        {
-          test: /\.json$/,
-          include: /node_modules/,
-          loader: 'json-loader'
-        }
-      ]
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf)$/, loader: 'url-loader', options: { name: '[hash].[ext]', limit: 10000 } },
+      { test: /\.ico$/, loader: 'file-loader', options: { name: '[hash].[ext]' } },
+      {
+        test: /(\.css|\.scss)$/,
+        use: [
+          "style-loader",
+          // MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: false }
+          },
+          {
+            loader: "postcss-loader",
+            options: { sourceMap: false }
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: false }
+          }
+        ]
+        // loaders: [
+        //   'style-loader', 
+        //   'css-loader', 
+        //   'sass-loader']
+
+      },
+      {
+        test: /\.json$/,
+        include: /node_modules/,
+        loader: 'json-loader'
+      }
+    ]
   }
 };

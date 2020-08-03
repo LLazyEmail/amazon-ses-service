33fb09b765c0f211e3a06771a990a23601dadb50
Vadim <vadim.putrov@gmail.com>
2020-08-03 18:32:17 +0300

    add splitting psql redis urls

--- server/config/sequelize_config.js
+++ server/config/sequelize_config.js
@@ -1,4 +1,48 @@
-module.exports = {
+const urlPSQL = process.env.DATABASE_URL;
+
+let production = {
+    "username": process.env.PSQL_USERNAME,
+    "password": process.env.PSQL_PASSWORD,
+    "database": process.env.PSQL_DATABASE,
+    "host": process.env.PSQL_HOST || "127.0.0.1",
+    "dialect": "postgres",
+    "logging": false,
+    "url": process.env.DATABASE_URL,
+    "ssl": process.env.HEROKU ? true : false,
+    "dialectOptions": process.env.HEROKU ? { "ssl": true } : {}
+};
+
+
+if (urlPSQL) {
+  const PSQLCreds = urlPSQL.slice(11);
+  console.log("pSQLCres", PSQLCreds);
+  const splittedDataPSQL = PSQLCreds.split(':');
+
+  const username = splittedDataPSQL[0];
+
+  const splittedPassHost = splittedDataPSQL[1].split('@');
+  const password = splittedPassHost[0];
+  const host = splittedPassHost[1];
+
+  const splittedHostDB = splittedDataPSQL[2].split('/');
+
+  const port = splittedHostDB[0];
+  const database = splittedHostDB[1];
+
+  console.log("username", username);
+  console.log("host", host);
+  production = {
+    ...production,
+    username,
+    password,
+    database,
+    host
+  };
+}
+
+console.log(production);
+
+const dataPSQL = {
   "development": {
     "username": process.env.PSQL_USERNAME,
     "password": process.env.PSQL_PASSWORD,
@@ -16,15 +60,8 @@ module.exports = {
     "dialect": "postgres",
     "logging": false
   },
-  "production": {
-    "username": process.env.PSQL_USERNAME,
-    "password": process.env.PSQL_PASSWORD,
-    "database": process.env.PSQL_DATABASE,
-    "host": process.env.PSQL_HOST || "127.0.0.1",
-    "dialect": "postgres",
-    "logging": false,
-    "url": process.env.DATABASE_URL, 
-    "ssl": process.env.HEROKU ? true : false,
-    "dialectOptions": process.env.HEROKU ? {"ssl":true} : {}
-  }
+  production
 };
+
+
+module.exports = dataPSQL;
--- server/config/server/redis.js
+++ server/config/server/redis.js
@@ -1,12 +1,32 @@
 const redis = require('redis');
 
 module.exports = () => {
-  const redisSettings = {
+  let redisSettings = {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD,
   };
   const redisUrl = process.env.REDIS_URL;
+
+  if(redisUrl){
+    const cred = redisUrl.slice(8);
+    const splittedDataCreds = cred.split(':');
+
+    const username = splittedDataCreds[0];
+    const port = splittedDataCreds[2];
+    const splittedPassHost = splittedDataCreds[1].split('@');
+
+    const password = splittedPassHost[0];
+    const host = splittedPassHost[1];
+
+
+    redisSettings = {
+      host,
+      port,
+      password
+    };
+  }
+
 
   const client = createRedisClient(redisSettings,redisUrl); 
   const subscriber = createRedisClient(redisSettings,redisUrl);  // Need to create separate connections
